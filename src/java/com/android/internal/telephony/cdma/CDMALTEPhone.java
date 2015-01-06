096aa2e027a46d5f59e3d1a2746e834bd293536d
nuclearmistake <nuclearmistake@gmail.com>
2015-01-05 19:09:26 -0500

    CDMALTEPhone didn't have this case before oosisdc handling, so it shouldn't after.
    Change-Id: Ibe55ff26076025fa6d3e29dd9d5a6e129e16462d

--- src/java/com/android/internal/telephony/cdma/CDMALTEPhone.java
+++ src/java/com/android/internal/telephony/cdma/CDMALTEPhone.java
@@ -149,10 +149,6 @@ public class CDMALTEPhone extends CDMAPhone {
             // removeReferences() have already been called
 
             ret = PhoneConstants.DataState.DISCONNECTED;
-        } else if (mSST.getCurrentDataConnectionState() != ServiceState.STATE_IN_SERVICE &&
-                            mOosIsDisconnect) {
-            ret = PhoneConstants.DataState.DISCONNECTED;
-            log("getDataConnectionState: Data is Out of Service. ret = " + ret);
         } else if (mDcTracker.isApnTypeEnabled(apnType) == false) {
             ret = PhoneConstants.DataState.DISCONNECTED;
         } else {
